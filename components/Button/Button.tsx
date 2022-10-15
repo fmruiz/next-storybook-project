1ba78d3d6a31d313631270528c2b1e5850b80ff0
fmruiz <x.francoruiz@gmail.com>
2022-10-15 12:10:43 -0300

    worked in neumorphic Button component

--- components/Button/Button.tsx
+++ components/Button/Button.tsx
@@ -1,6 +1,42 @@
+import { MouseEvent } from 'react';
 import styled from '@emotion/styled';
+import { css, SerializedStyles } from '@emotion/react';
 
-export const Button = styled.button`
+export type Colors = 'primary' | 'secondary' | 'danger' | 'warning';
+
+export type Props = {
+    children: string;
+    color?: Colors;
+    onClick: (event: MouseEvent<HTMLButtonElement>) => void;
+};
+
+export const getColors = (color?: Colors): SerializedStyles => {
+    switch (color) {
+        case 'primary':
+            return css`
+                background: #6d5dfc;
+                color: #e4ebf5e6;
+            `;
+        case 'secondary':
+            return css`
+                color: #5e5c64e6;
+            `;
+        case 'danger':
+            return css`
+                background: #dc3545e6;
+                color: #e4ebf5e6;
+            `;
+        case 'warning':
+            return css`
+                background: #ffca2ce6;
+                color: #5e5c64e6;
+            `;
+        default:
+            return css``;
+    }
+};
+
+export const Button = styled.button<Props>`
     all: unset;
     display: flex;
     justify-self: center;
@@ -13,8 +49,17 @@ export const Button = styled.button`
     height: 4rem;
     border-radius: 1rem;
     transition: all 0.4s ease;
+    ${({ color }) => getColors(color)}
+    box-shadow: 0.5vmin 0.5vmin 1vmin #c8d0e7, -0.5vmin -0.5vmin 1vmin #ffffff;
     &:hover {
         opacity: 0.9;
     }
-    box-shadow: 0.5vmin 0.5vmin 1vmin #c8d0e7, -0.5vmin -0.5vmin 1vmin #ffffff;
+    &:active {
+        box-shadow: 0.5vmin 0.5vmin 1vmin #c8d0e7 inset,
+            -0.5vmin -0.5vmin 1vmin #ffffff inset;
+    }
 `;
+
+Button.defaultProps = {
+    color: 'primary',
+};
